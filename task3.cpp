e739670e52e0b4a6cb328a18192a1d1505847853
NdetecT306 <nstasya777@mail.ru>
2025-06-13 17:19:47 +0700

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -3,50 +3,34 @@
 #include <numeric>
 #include <iomanip>
 using namespace std;
-double sum(int a, int b, int rounds = 1000) 
-{ 
+double sum(int a, int b, int rounds = 1000) { 
     double sum = 0;
     for (int n = 1; n <= rounds; ++n) {
         sum += pow(n, a) / pow(b, n);
     }
     return sum;
 }
-int NOD(int a, int b)
-{
-    if (a % b == 0)
-        return b;
-    if (b % a == 0)
-        return a;
-    if (a > b)
-        return NOD(a % b, b);
-    return NOD(a, b % a);
-}
 int main() {
     int a, b;
     cin >> a >> b;
-    if (a < 1 || a > 10 || b < 1 || b > 10)
-    {
+    if (a < 1 || a > 10 || b < 1 || b > 10){ //Базовый случай
         cout << "Неверный ввод" << endl;
         return 1;
     }
-    if (b <= 1 && a > 0)
-    {
+    if (b <= 1 && a > 0){ //Бесконечность
         cout << "infinity" << endl;
         return 1;
     }
     double summa = sum(a, b);
-    double checks = 10;
+    double checks = 10000;
     double num = round(summa * checks);
-    double denominator = checks;
-    double common_divisor = NOD(num, denominator);
-    num /= common_divisor;
-    denominator /= common_divisor;
-    if (abs(summa - num / denominator) < 1) {
-        cout << num << "/" << denominator << endl;
+    double denom= checks;
+    if (abs(summa - num / denom) < 1) {
+        cout << num << "/" << denom << endl;
     }
-    else
-    {
+    else{
         cout << "irrational" << endl;
     }
     return 0;
 }
+
